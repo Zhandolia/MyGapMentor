4892aa834db80c2ff411090821a8b4fadd077cce
Zhandos <zhandosbrown@gmail.com>
2023-08-31 19:46:43 +0600

    dobavil animation 1.0

--- src/home.js
+++ src/home.js
@@ -1,49 +1,3 @@
-// import React, { useEffect } from 'react';
-// import './App.css';
-// import {Link} from "react-router-dom";
-// import logo from './favicon.ico';
-
-// function Home() {
-//     return (
-//         <div id="grad1" className="App">
-//             <div id="navbar" className="navbar">
-//                 <div className="navbar-left">
-//                     <div className="home-logo-group">
-//                         <a href="/">
-//                             <img src={logo} alt="Logo" className="navbar-logo" />
-//                         </a>
-//                         <a href="/">MyGapMentor</a>
-//                     </div>
-//                 </div>
-//                 <div className="navbar-center">
-//                     <a href="about">About us</a>
-//                     <a href="stories">Success Stories</a>
-//                     <a href="plans">Plans</a>
-//                     <a href="contact">Contact us</a>
-//                 </div>
-
-//                 <div className="navbar-right">
-//                     <a href="account">Account</a>
-//                 </div>
-//             </div>
-
-//             <h2>Welcome to MyGapMentor.com</h2>
-//             <h3 id='gateway'>Your Gateway to Maximizing Your Gap Year Potential!</h3>
-
-//             <div className='basicdiv'>
-//                 <a href="/basics" className="big-button-link">
-//                     <button className="big-button">Ready to Start Planning Your Resume with us? Click Here!</button>
-//                 </a>
-//                 <Link to="about" className="learn-about-button">Learn more About us &#8594;</Link>
-//                 <Link to="plans" className="learn-plans-button">Get to know our Plans &#8594;</Link>
-//                 <h3 className="nFac">Powered by nFactorial 2023</h3>
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default Home;
-
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import {Link} from "react-router-dom";
@@ -95,7 +49,7 @@ function Home() {
                 </div>
             </div>
 
-            <h2>Welcome to MyGapMentor.com</h2>
+            <h2>Welcome to MyGapMentor</h2>
             <h3 id='gateway'>Your Gateway to Maximizing Your Gap Year Potential!</h3>
 
             <div className='basicdiv'>
